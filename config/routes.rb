deeaa2febe28b70687eab2c19efb2dce2f6c18c3
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-13 19:56:09 -0400

    add authenticate user, add routes for users, classrooms, and students

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  
+  root 'users#index'
+
+  resources :users do 
+  	resources :classrooms 
+  end 
+
+  resources :students 
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
