324f1e3f5d310a3a65cec1e71c35b0d33b186954
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-14 12:19:34 -0400

    add controller actions for student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,54 @@
-class StudentsController < ApplicationController 
+class StudentsController < ApplicationController
 	def index 
+		@students = Student.all
+		@student = Student.find_by(id:params[:id])
 	end 
-end 
+
+	def new 
+		@student = Student.new 
+		@user = current_user
+	end
+
+	def create 
+		@student = Student.new(student_params)
+		@classroom = Classroom.find_by(id:params[:id])
+		if @student.valid? 
+			@student.save 
+			redirect_to student_path(@student)
+		else 
+			render 'new'
+		end
+	end
+
+	def show 
+		@student = Student.find_by(id:params[:id])
+	end 
+
+	def edit 
+		@student = Student.find_by(id:params[:id])
+	end 
+
+	def update 
+		@student = Student.find_by(id:params[:id])
+		@classroom = Classroom.find_by(id:params[:id])
+		if @student.valid? 
+			@student.update(student_params)
+			redirect_to student_path(@student)
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy 
+		@user = current_user
+		@student = Student.find_by(id:params[:id])
+		@student.destroy
+		redirect_to user_path
+	end
+
+	private
+	def student_params 
+		params.require(:student).permit(:first_name, :last_name, :grade, classroom_ids:[])
+	end 
+
+end
