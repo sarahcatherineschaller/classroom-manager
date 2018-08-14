57287c57f4f9514ba1b38e6fd87640b35d864351
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-14 12:24:37 -0400

    add validations to student and classroom

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -2,4 +2,12 @@ class Classroom < ApplicationRecord
 	belongs_to :user
 	has_many :student_classrooms 
 	has_many :students, through: student_classrooms 
+
+	validates :subject, presence: true, uniqueness: true 
+
+	before_validation :make_title_case 
+
+	def make_title_case 
+		self.subject = self.subject.titlecase 
+	end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ApplicationRecord
 	has_many :users, through: :classrooms 
 	has_many :student_classrooms 
 	has_many :classrooms, through: :student_classrooms
+
+	validates :first_name, :last_name, :grade, presence: true 
+
+	before_validation :make_title_case 
+
+	def make_title_case 
+		self.first_name = self.first_name.titlecase 
+		self.last_name = self.last_name.titlecase 
+	end
 end
