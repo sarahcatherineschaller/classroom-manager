d8d0e1145c290b1334999357999ab9003aa68d75
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-14 16:36:22 -0400

    attempt to change github signup password

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   	resources :classrooms 
   end
 
+
   resources :students 
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
