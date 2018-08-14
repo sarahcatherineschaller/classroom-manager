3611447705633707518bedda6b32a3aed9ec7c4b
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-14 11:41:55 -0400

    add title and name to new user registration and edit form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable 
 
+  validates :title, inclusion: { in: %w(Mr. Ms. Mrs.), message: "(Please use Mr., Ms., or Mrs.)"}
+  validates :title, :first_name, :last_name, :email, :password, presence: true
+  validates :email, uniqueness: true
+
 
   def self.from_omniauth(auth) 
   	where(provider: auth.provider, uid: auth.uid).first_or_create do |user| 
