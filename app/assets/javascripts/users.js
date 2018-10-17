67170cde64ce9fe02a1a778868e1b8b04506a7d4
Sarah Schaller <sarahcschaller@gmail.com>
2018-10-16 20:53:45 -0400

    hijack submit event on classroom form

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,5 @@
 $(function() {
+	//load student index on users page
 	$("a.load_students").on("click", function(e) {
 		$.get(this.href).done(function(data) {
 
@@ -12,19 +13,25 @@ $(function() {
 
 	});
 
+	//load classroom index on users page
 	$("a.load_classrooms").on("click", function(e) {
 		$.get(this.href).done(function(data) {
 		
 			var $classrooms = $("div.classrooms")
 			$classrooms.html("")
 			data.forEach(function(classroom) {
-				 
 				$classrooms.append('<a href="users/' + classroom.user_id + '/classrooms/' + classroom.id + '">' + classroom.subject + "</a><br>");
 			});
 
 		});
+		e.preventDefault();
+	});
+
+	//submit classroom via AJAX
+	$("#new_classroom").on("submit", function(e) {
+		alert("you clicked submit!");
 
 		e.preventDefault();
-	})
+	});
 
 });
